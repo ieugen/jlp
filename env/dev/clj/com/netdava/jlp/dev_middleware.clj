9afdad2b2c623bc8c0ea2b0af6711f17662b285b
Eugen Stan <eugen.stan@netdava.com>
2022-09-09 21:45:57 +0300

    Initial commit

---
+++ env/dev/clj/com/netdava/jlp/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns com.netdava.jlp.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ test/clj/com/netdava/jlp/core_test.clj
@@ -0,0 +1,8 @@
+(ns com.netdava.jlp.core-test
+  (:require
+    [com.netdava.jlp.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
